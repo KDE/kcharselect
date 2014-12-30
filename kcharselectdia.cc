372163b0d6ffa40988e4808922565f4edefa2377
Montel Laurent <montel@kde.org>
2014-12-30 17:28:43 +0100

    Port to new connect api

--- kcharselectdia.cc
+++ kcharselectdia.cc
@@ -57,12 +57,11 @@ KCharSelectDia::KCharSelectDia()
   charSelect->setCurrentChar(vChr);
   charSelect->setCurrentFont(vFont);
   charSelect->resize(charSelect->sizeHint());
-  connect(charSelect,SIGNAL(currentCharChanged(const QChar &)),
-	  SLOT(charChanged(const QChar &)));
-  connect(charSelect,SIGNAL(charSelected(const QChar &)),
-	  SLOT(add(const QChar &)));
-  connect(charSelect,SIGNAL(currentFontChanged(const QFont &)),
-	  SLOT(fontSelected(const QFont &)));
+  connect(charSelect, &KCharSelect::currentCharChanged, this, &KCharSelectDia::charChanged);
+  connect(charSelect,SIGNAL(charSelected(QChar)),
+         SLOT(add(QChar)));
+
+  connect(charSelect, &KCharSelect::currentFontChanged, this, &KCharSelectDia::fontSelected);
   grid->addWidget(charSelect, 0, 0, 1, 4);
 
   // Build line editor
@@ -72,14 +71,13 @@ KCharSelectDia::KCharSelectDia()
 
   lined->setFont( vFont );
 
-  connect(lined,SIGNAL(textChanged(const QString &)),
-	  SLOT(lineEditChanged()));
+  connect(lined, &QLineEdit::textChanged, this, &KCharSelectDia::lineEditChanged);
   grid->addWidget(lined, 1, 0, 1, 3);
 
   bClip = new QPushButton( i18n( "&To Clipboard" ), mainWidget );
   bClip->setIcon( QIcon::fromTheme( QLatin1String( "edit-copy" ) ));
   bClip->setFixedSize( bClip->sizeHint() );
-  connect(bClip,SIGNAL(clicked()),this,SLOT(toClip()));
+  connect(bClip, &QPushButton::clicked, this, &KCharSelectDia::toClip);
   grid->addWidget(bClip, 1, 3);
 
   // Build menu
@@ -88,35 +86,35 @@ KCharSelectDia::KCharSelectDia()
   QAction *action = actionCollection()->addAction( QLatin1String( "copy_clip" ) );
   action->setText( i18n("&To Clipboard") );
   action->setIcon( QIcon::fromTheme( QLatin1String( "edit-copy" )) );
-  connect(action, SIGNAL(triggered(bool)), SLOT(toClip()));
+  connect(action, &QAction::triggered, this, &KCharSelectDia::toClip);
   actionCollection()->setDefaultShortcuts(action, KStandardShortcut::shortcut(KStandardShortcut::Copy));
 
   action = actionCollection()->addAction( QLatin1String( "copy_utf_8" ) );
   action->setText( i18n("To Clipboard &UTF-8") );
-  connect(action, SIGNAL(triggered(bool) ), SLOT(toClipUTF8()));
+  connect(action, &QAction::triggered, this, &KCharSelectDia::toClipUTF8);
   action = actionCollection()->addAction( QLatin1String( "copy_html" ) );
   action->setText( i18n("To Clipboard &HTML") );
-  connect(action, SIGNAL(triggered(bool) ), SLOT(toClipHTML()));
+  connect(action, &QAction::triggered, this, &KCharSelectDia::toClipHTML);
 
   action = actionCollection()->addAction( QLatin1String( "from_clip" ) );
   action->setText( i18n("&From Clipboard") );
   action->setIcon( QIcon::fromTheme( QLatin1String( "edit-paste" )) );
-  connect(action, SIGNAL(triggered(bool)), SLOT(fromClip()));
+  connect(action, &QAction::triggered, this, &KCharSelectDia::fromClip);
   actionCollection()->setDefaultShortcuts(action, KStandardShortcut::shortcut(KStandardShortcut::Paste));
   action = actionCollection()->addAction( QLatin1String( "from_clip_utf8" ) );
   action->setText( i18n( "From Clipboard UTF-8") );
-  connect(action, SIGNAL(triggered(bool) ), SLOT(fromClipUTF8()));
+  connect(action, &QAction::triggered, this, &KCharSelectDia::fromClipUTF8);
 
   i18n("From Clipboard HTML");      // Intended for future use
 
   action = actionCollection()->addAction( QLatin1String( "flip" ) );
   action->setText( i18n("&Flip Text") );
-  connect(action, SIGNAL(triggered(bool) ), SLOT(flipText()));
+  connect(action, &QAction::triggered, this, &KCharSelectDia::flipText);
 
   action = new KToggleAction( i18n("&Reverse Direction"), this );
   action->setChecked(_rtl);
   actionCollection()->addAction( QLatin1String( "rtl" ), action );
-  connect(action, SIGNAL(toggled(bool) ), SLOT(setRtl(bool)));
+  connect(action, &QAction::toggled, this, &KCharSelectDia::setRtl);
 
   charSelect->setFocus();
 
