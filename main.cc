8e8dbc48e16b6f54c3daa747df59a6be11f09d07
Christoph Cullmann <cullmann@kde.org>
2015-07-30 10:24:26 +0200

    CHANGELOG: KCharSelect supports HiDPI

--- main.cc
+++ main.cc
@@ -27,6 +27,11 @@ int main(int argc, char **argv)
 {
   QApplication app( argc, argv );
 
+  /**
+   * enable high dpi support
+   */
+  app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+
   Kdelibs4ConfigMigrator migrate(QLatin1String("kcharselect"));
   migrate.setConfigFiles(QStringList() << QLatin1String("kcharselectrc"));
   migrate.setUiFiles(QStringList() << QLatin1String("kcharselectui.rc"));
